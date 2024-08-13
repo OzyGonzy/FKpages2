fab47c30479eaeec67d77f93f52f87b8e3123ee5
Ozy Gonzy <74786855+OzyGonzy@users.noreply.github.com>
2024-08-13 14:59:48 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,6 @@ mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Alterna la clase 'active' en el menú
 });
 
-
 let currentSlide = 0;
 
 function changeSlide(direction) {
@@ -17,6 +16,14 @@ function changeSlide(direction) {
     document.querySelector('.carousel-images').style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Si el usuario ha desplazado más de 50px
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
 
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita el envío del formulario
